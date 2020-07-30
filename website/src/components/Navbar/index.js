171dbab2175127ebbb4a657577bc21a4f07f8d0b
Cristian Cahe <cgcahe@gmail.com>
2020-07-30 14:49:49 -0300

    Clean

--- website/src/components/Navbar/index.js
+++ website/src/components/Navbar/index.js
@@ -6,9 +6,13 @@ import ContactButton from './contactButton';
 export default function Navbar() {
   return (
     <div className="navbar">
-      <img src={Logo} alt="logo" tabIndex="0" />
-      <span>*drawing created by <a href="https://www.twitter.com/kojaqk" rel="noreferrer noopener" target="_blank">@kojaqk</a></span>
-      <ContactButton tabIndex="1" />
+      <div className="logo">
+        <img src={Logo} alt="logo" tabIndex="0" />
+        <span>*drawing created by <a href="https://www.twitter.com/kojaqk" rel="noreferrer noopener" target="_blank">@kojaqk</a></span>
+      </div>
+      <div className="button">
+        <ContactButton tabIndex="1" />
+      </div>
     </div>
   );
 }
